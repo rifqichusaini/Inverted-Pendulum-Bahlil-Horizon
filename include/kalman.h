3d5aaeb59fbb2eae68297b3c97fc0e8548983374
Rifqi Chusaini <mrifqichusainis@gmail.com>
2025-11-24 23:15:48 +0700

    update: ganti size tab dan tukar private keatas.

--- include/kalman.h
+++ include/kalman.h
@@ -1,36 +1,36 @@
 #pragma once
 
 class Kalman1D {
-public:
-  Kalman1D(float q = 0.001f, float r = 0.03f, float initial = 0.0f) {
-    Q = q; R = r; x = initial;
-    P = 1.0f; // initial estimation error
-  }
+  private:
+    float Q; // process noise covariance
+    float R; // measurement noise covariance
+    float x; // state estimate
+    float P; // estimation error covariance
 
-  void setQ(float q){ Q = q; }
-  void setR(float r){ R = r; }
-  float getState(){ return x; }
+  public:
+    Kalman1D(float q = 0.001f, float r = 0.03f, float initial = 0.0f) {
+      Q = q; R = r; x = initial;
+      P = 1.0f; // initial estimation error
+    }
 
-  // z = measurement
-  float update(float z){
-    // Predict step
-    P = P + Q;
+    void setQ(float q){ Q = q; }
+    void setR(float r){ R = r; }
+    float getState(){ return x; }
 
-    // Kalman gain
-    float K = P / (P + R);
+    // z = measurement
+    float update(float z){
+      // Predict step
+      P = P + Q;
 
-    // Update estimate with measurement z
-    x = x + K * (z - x);
+      // Kalman gain
+      float K = P / (P + R);
 
-    // Update error covariance
-    P = (1 - K) * P;
+      // Update estimate with measurement z
+      x = x + K * (z - x);
 
-    return x;
-  }
+      // Update error covariance
+      P = (1 - K) * P;
 
-private:
-  float Q; // process noise covariance
-  float R; // measurement noise covariance
-  float x; // state estimate
-  float P; // estimation error covariance
+      return x;
+    }
 };
